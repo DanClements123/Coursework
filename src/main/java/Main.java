128b9321cda277e973dc8e3232d9c210265c0d32
Daniel <87710@farnborough.ac.uk>
2019-09-09 16:56:21 +0100

    Merge branch 'master' of https://github.com/DanClements123/Coursework
    # Conflicts:
    #	.idea/compiler.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    //----------------------------------------------------------------
+
     public static Connection db = null;
 
 
@@ -42,23 +44,40 @@ public class Main {
 
     }
 
+    //----------------------------------------------------------------
+
+
     public static void main(String[] args) {
+
+//Input into data base
 
         openDatabase("Users.db");
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+
+        System.out.println("Enter your first name: ");
+        String firstNameInput = input.nextLine();
+
+        System.out.println("Enter your sure name: ");
+        String lastNameInput = input.nextLine();
+
+        System.out.println("Please choose your username: ");
         String userNameInput = input.nextLine();
+
         System.out.println("Enter your user ID number: ");
         int userIdInput = input.nextInt();
 
+//INPUT STATEMENT
 
         try {
 
-            PreparedStatement ps = db.prepareStatement("INSERT INTO userInfo (userID, userName) VALUES (?, ?)");
+            PreparedStatement ps = db.prepareStatement("INSERT INTO userInfo (userID, userName, firstName, lastName) VALUES (?, ?, ?, ?)");
 
             ps.setInt(1, userIdInput);
             ps.setString(2, userNameInput);
+            ps.setString(3, firstNameInput);
+            ps.setString(4, lastNameInput);
+
             ps.executeUpdate();
 
             //code using the database goes here
@@ -67,17 +86,63 @@ public class Main {
         } catch (Exception exception){
             System.out.println("Database disconnection error: " + exception.getMessage());
         }
+// DELETE STATEMENT
 
         try {
 
-            PreparedStatement ps = db.prepareStatement("SELECT userID, userName FROM userInfo");
+            PreparedStatement ps = db.prepareStatement("DELETE FROM userInfo WHERE userName = 12");
+            ps.execute();
+            //code using the database goes here
+
+
+        } catch (Exception exception){
+            System.out.println("Database disconnection error: " + exception.getMessage());
+        }
+
+// CREATE STATEMENT
+
+        try {
+
+            PreparedStatement ps = db.prepareStatement("CREATE TABLE test_table(column_1 datatype, column_2 datatype, column_3 datatype) ");
+            ps.execute();
+            //code using the database goes here
+
+
+        } catch (Exception exception){
+            System.out.println("Database disconnection error: " + exception.getMessage());
+        }
+
+// UPDATE STATEMENT
+
+        try {
+
+            PreparedStatement ps = db.prepareStatement("UPDATE table_name SET test_column = test_value WHERE test_column = test_value");
+            ps.execute();
+            //code using the database goes here
+
+        } catch (Exception exception){
+            System.out.println("Database disconnection error: " + exception.getMessage());
+        }
+
+
+
+
+
+
+
+        // OUTPUT STATEMENT
+        try {
+
+            PreparedStatement ps = db.prepareStatement("SELECT userID, userName, firstName, lastName FROM userInfo");
 
             ResultSet results = ps.executeQuery();
             while(results.next()){
 
                 int userID = results.getInt(1);
-                String username = results.getString(2);
-                System.out.println(userID + " " + username);
+                String userName = results.getString(2);
+                String firstName = results.getString(3);
+                String lastName = results.getString(4);
+                System.out.println(userID + " " + userName + " " + firstName + " " + lastName);
 
             }
 
