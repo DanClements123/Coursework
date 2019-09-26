123310f53a29c4e26d5abdaf9e7c861771874c1e
Daniel <87710@farnborough.ac.uk>
2019-09-26 20:52:01 +0100

    Merge branch 'master' of https://github.com/DanClements123/Coursework

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -51,9 +51,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-//Input into data base
+            openDatabase("things.db");
 
-        openDatabase("Users.db");
+            ResourceConfig config = new ResourceConfig();
+            config.packages("Controllers");
+            config.register(MultiPartFeature.class);
+            ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+            Server server = new Server(8081);
+            ServletContextHandler context = new ServletContextHandler(server, "/");
+            context.addServlet(servlet, "/*");
+
+            try {
+                server.start();
+                System.out.println("Server successfully started.");
+                server.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+        //Input into data base
 
         Scanner input = new Scanner(System.in);
 
