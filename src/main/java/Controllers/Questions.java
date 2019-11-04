012a05a730e8e6319ef4eda4292c4dcdd2f39e44
DanClements123 <87710@farnborough.ac.uk>
2019-11-04 16:09:32 +0000

    Second test from college pcs

--- src/main/java/Controllers/Questions.java
+++ src/main/java/Controllers/Questions.java
@@ -1,4 +1,142 @@
 package Controllers;
+package Controllers;
+import Server.Main;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Questions {
+
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listThings() {
+        System.out.println("thing/list");
+        JSONArray list = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT questionID, question, correctAnswer, wrongAnswer, wrongAnswer2, wrongAnswer3, topicID");
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                JSONObject item = new JSONObject();
+                item.put("questionID", results.getInt(1));
+                item.put("question", results.getString(2));
+                item.put("correctAnswer", results.getString(3));
+                item.put("wrongAnswer1", results.getString(4));
+                item.put("wrongAnswer2", results.getString(5));
+                item.put("wrongAnswer3", results.getString(6));
+                item.put("topicID", results.getInt(7));
+            }
+            return list.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+        }
+    }
+
+
+    @GET
+    @Path("get/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getThing(@PathParam("id") Integer id) {
+        if (id == null) {
+            throw new Exception("Thing's 'id' is missing in the HTTP request's URL.");
+        }
+        System.out.println("thing/get/" + id);
+        JSONObject item = new JSONObject();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT question FROM Questions WHERE questionID = ? ");
+            ps.setInt(1, id);
+            ResultSet results = ps.executeQuery();
+            if (results.next()) {
+                item.put("questionID", id);
+                item.put("")
+            }
+            return item.toString();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // end of file
+
+
+
+
