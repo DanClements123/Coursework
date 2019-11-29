307bffb549bdf6d59b100d8be213bfc04ad8b965
DanClements123 <87710@farnborough.ac.uk>
2019-11-29 12:12:18 +0000

    College commit

--- resources/Client/js/index.js
+++ resources/Client/js/index.js
@@ -4,7 +4,7 @@ function pageLoad() {
 
     let myHTML = '<div style="text-align:center;">'
         + '<h1>Welcome to my API powered website!</h1>'
-        + '<img src="/client/img/demo.png"  alt="Logo"/>'
+        + '<img src="/client/img/quiz-logo"  alt="Logo"/>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>'
