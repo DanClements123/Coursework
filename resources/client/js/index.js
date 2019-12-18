bb048e1805d449d301ef3c7ca8d678bd87e6cf12
DanClements123 <87710@farnborough.ac.uk>
2019-12-18 09:38:25 +0000

    College commit

---
+++ resources/client/js/index.js
@@ -0,0 +1,9 @@
+function pageLoad() {
+
+    let testHTML = '<div style="text-align:right;">'
+        + '<img src="/client/img/quiz-logo.jpg" alt="Logo"/>'
+
+
+    document.getElementById("testDiv").innerHTML = testHTML;
+
+}
