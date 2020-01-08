044aa8956c3db39edd7849ed24e8add1296e5744
Daniel <87710@farnborough.ac.uk>
2020-01-08 20:46:25 +0000

    Home commit 30/11/2019

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,9 +1,13 @@
-function image1() {
-
-    let testHTML = '<div style="text-align:right; position:absolute;" >'
+function logos() {
+    let testHTML = '<div style="text-align:right; position:fixed;" >'
         + '<img src="/client/img/quiz-logo.jpg" style="height:105px" alt="Logo"/>';
-
     document.getElementById("testDiv").innerHTML = testHTML;
-
 }
 
+/*
++ '<img src="/client/img/Cartoon%20Computer.jpg" style="height:105px" alt="Logo2"/>';
++ '<img src="/client/img/english.jpg" style="height:105px" alt="Logo3"/>';
++ '<img src="/client/img/History%20img.png" style="height:105px" alt="Logo4"/>';
++ '<img src="/client/img/Politics.jpg" style="height:105px" alt="Logo5"/>';
++ '<img src="/client/img/Science.png" style="height:105px" alt="Logo6"/>';
+*/
